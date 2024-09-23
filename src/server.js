a2c96cb1cdbdd96d7721f2a80b7b9fa3f1e5707a
nikhilka <nikhilka23@gmail.com>
2024-09-23 12:57:53 +0530

    Aggregation to include todo stats on welcome page

--- src/server.js
+++ src/server.js
@@ -4,18 +4,39 @@ require("dotenv").config();
 
 const connectDB = require("../dbConnect.js");
 const toDoRoutes = require("./routes/toDoRoutes");
+const ToDo = require("./models/ToDoModel.js");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 connectDB();
-app.get("/", (req, res) => {
-	res
-		.status(200)
-		.send(
-			"<h1>Welcome to To Do App built with NodeJS, ExpressJS & MongoDB</h1>"
-		);
+app.get("/", async (req, res) => {
+	const TotalItemsinDB = await ToDo.aggregate([{ $count: "Total Todos" }]);
+	const completionStatus = await ToDo.aggregate([
+		{
+			$group: {
+				_id: "$status",
+				Total: { $sum: 1 },
+			},
+		},
+	]);
+
+	const typesToDos = await ToDo.aggregate([
+		{
+			$group: {
+				_id: "$type",
+				Total: { $sum: 1 },
+			},
+		},
+	]);
+
+	res.status(200).json({
+		title: "Welcome to To Do App built with NodeJS, ExpressJS & MongoDB",
+		Total_Number_Of_ToDos: TotalItemsinDB,
+		Completion_Status: completionStatus,
+		ToDo_Types: typesToDos,
+	});
 });
 
 app.use("/todos", toDoRoutes);
